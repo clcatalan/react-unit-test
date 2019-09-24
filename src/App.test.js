5a7db6d11f82520f040bdb8c99dbc9572bd28083
Carlos Rafael Catalan <carlos.catalan@whitecloak.com>
2019-09-24 17:20:21 +0800

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {sum} from './math';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('Examining the syntax of Jest tests', () => {
+   
+  it('sums numbers', () => {
+      expect(sum(1,2)).toEqual(3);
+      expect(sum(2,2)).toEqual(4);
+   });
 });
